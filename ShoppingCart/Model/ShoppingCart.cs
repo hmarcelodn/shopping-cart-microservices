7c7bf587d0717f53b91b1ace3da166d75a5dc09a
Hugo Marcelo Del Negro <hdelnegro@belatrix.com>
2016-12-19 03:30:29 -0300

    Adding Recommendations, ShopperTracking and ProductsCatalog Microservices

--- ShoppingCart/Model/ShoppingCart.cs
+++ ShoppingCart/Model/ShoppingCart.cs
@@ -1,14 +1,19 @@
 ﻿using ShoppingCarts.Microservice.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace ShoppingCarts.Microservice.Model
 {
     public class ShoppingCart
     {
-        public void AddItems(IList<ShoppingCartItem> shoppingCartItem, IEventStore eventStore)
-        { }
+        public void AddItems(IEnumerable<ShoppingCartItem> shoppingCartItem, IEventStore eventStore)
+        {
+            throw new NotImplementedException();
+        }
 
         public void RemoveItems(int[] productCatalogIds, IEventStore eventStore)
-        { }
+        {
+            throw new NotImplementedException();
+        }
     }
 }
